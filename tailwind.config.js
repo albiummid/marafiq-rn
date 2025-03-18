44921497a806dc56a8a68e893ffcde868cf0f7ad
albiummid <albi.ummid@gmail.com>
2025-03-18 17:14:38 +0600

    assets and colors added.

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,17 +17,56 @@ module.exports = {
       },
 
       colors: {
-        primary: '#064145',
-        title: '#272727',
-        subT: '#5e5e5e',
-        primary: '#064145',
-        offWhite: '#E6ECEC',
-        secondary: '#F4FAFA',
-        white100: '#EFEFEF',
-        border: '#DFDFDF',
-        primary100: '#EEF6F6',
-        primary200: '#9BB3B5',
-        danger: '#CE3535',
+        // neutral
+       primaryBG:"#E1E6EF",
+       baseBG:"#F8F9FC",
+       secondaryBG:"#F1F3F9",
+       line:"#E1E6EF",
+       lineDark:"#3F444D",
+       secondaryBGDark:"#23272F",
+       primaryBGDark:"#1B1F27",
+       baseBGDark:"#0A0D14",
+      //  green
+      green50:"#F1FBF2", //bg
+      green100:"#BBF1C4", //bg
+      green200:"#89E798",// default
+      green300:"#63B470",// hover
+      green600:"#2AA96F",//pressed
+      green900:"#244E2B",//pressed
+      green900Lite:"#89E798",//pressed
+      // alertPrimary
+      alertPrimary50:"#FFF8EB",
+      alertPrimary100:"#FFF1D6",
+      alertPrimary700:"#FEE2A9",
+      alertPrimary800:"#FDCF72",
+      alertPrimary900:"#FBBB3C",
+      // Alert Premium
+      alertPremium50:"#FFE9EA",
+      alertPremium100:"#FFA9AB",
+      alertPremium700:"#FF686D",
+      alertPremium800:"#F7272D",
+      alertPremium900:"#B30005",
+      alertPremium900Deep:"#6F0003",
+      // Text
+      textPrimary:"#1D2433",
+      textSecondary:"#4A505C",
+      textDisabled:"#6C717A",
+      textPrimaryDark:"#FFFFFF",
+      textSecondaryDark:"#DCDCDC",
+      textDisabledDark:"#C1C2C4",
+
+
+
+
+
+
+
+
+
+
+
+
+
       },
     },
   },
